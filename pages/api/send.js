d27782f22792c3d4621676b3415a4d3abd631b0d
JosiasAurel <ndjosiasaurel@gmail.com>
2021-02-14 09:50:27 +0100

    add mailing function

--- pages/api/send.js
+++ pages/api/send.js
@@ -1,5 +1,26 @@
+const mailer = require("nodemailer")
+
+let transporter = mailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "skisnippet@gmail.com",
+        pass: "snippetboyo500"
+    }
+})
+
+
 module.exports = (req, res) => {
     const msg = req.body
+
+    let mail = {
+    from: "skisnippet@gmail.com",
+    to: "ndjosiasaurel@gmail.com",
+    subject: "Valentine",
+    text: String(msg)
+}
+ transporter.sendMail(mail, function(err, data) {
+    if (err) console.log(err)
+    console.log("Sent")
+})
     console.log(msg)
-    res.send(msg)
 }
