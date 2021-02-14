5e98d77954d7f74292e86855a116c02cb870da5a
JosiasAurel <ndjosiasaurel@gmail.com>
2021-02-14 10:04:05 +0100

    remove nodemailer

--- pages/api/send.js
+++ pages/api/send.js
@@ -1,29 +1,6 @@
-const mailer = require("nodemailer")
-const smtp = require("nodemailer-smtp-transport")
-
-let transporter = mailer.createTransport(smtp(
-    {
-    service: "gmail",
-    auth: {
-        user: "skisnippet@gmail.com",
-        pass: "snippetboyo500"
-    }
-}
-))
 
 
 module.exports = (req, res) => {
     const msg = req.body
-
-    let mail = {
-    from: "skisnippet@gmail.com",
-    to: "ndjosiasaurel@gmail.com",
-    subject: "Valentine",
-    text: String(msg)
-}
- transporter.sendMail(mail, function(err, data) {
-    if (err) console.log(err)
-    console.log("Sent")
-})
     console.log(msg)
 }
